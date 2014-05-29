52eba9c47409efa7d0e970212465333f6f77af50
Paweł Obrok <pawel.obrok@gmail.com>
2014-05-29 16:50:41 +0200

    Allow for setting of a custom failure handler

--- lib/invariant.rb
+++ lib/invariant.rb
@@ -1,23 +1,32 @@
-module Invariant
+require "singleton"
+
+class Invariant
+  include Singleton
+
   class AssertionError < StandardError
   end
 
+  attr_accessor :failure_handler
+
   def self.enable_assertions
-    Kernel.class_eval do
-      def assert(first=nil, second=nil)
-        if block_given?
-          assert(yield, first)
-        else
-          raise AssertionError, second unless first
-        end
-      end
-    end
+    failure_handler { |msg| raise AssertionError.new(msg) }
   end
 
   def self.disable_assertions
-    Kernel.class_eval do
-      def assert(*args, &block)
-      end
+    failure_handler { }
+  end
+
+  def self.failure_handler(&block)
+    instance.failure_handler = block
+  end
+end
+
+Kernel.class_eval do
+  def assert(first=nil, second=nil)
+    if block_given?
+      assert(yield, first)
+    else
+      Invariant.instance.failure_handler.call(second) unless first
     end
   end
 end
